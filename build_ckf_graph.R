c917be1da524a12e5062fd160037d0256fc7709b
Brian Loyal <bloyal@gmail.com>
2014-12-22 20:20:52 -0600

    Create CKF menu graph
    Initial commit to create the Cheesecake Factory menu graph

---
+++ build_ckf_graph.R
@@ -0,0 +1,70 @@
+library(RNeo4j);
+
+#graph = startGraph("http://localhost:7474/db/data/");
+
+#graph$version;
+# [1] "2.1.6"
+
+#bulkCreateNodes(graph, "MenuItem","name",menu$name)
+#bulkCreateNodes(graph, "Feature","name",unique(unlist(features)))
+
+#bulkUpdateNodeProperties(graph, rep("MenuItem",323), 
+#                         rep("name",323), menu$name, 
+#                         rep("description",323), menu$description)
+
+# bulkCreateRelationships(graph, 
+#                          rep("MenuItem",3184), rep("name",3184), df$menuItems,
+#                          rep("Feature",3184), rep("name",3184), df$features,
+#                          rep("HAS_FEATURE",3184)
+#                          )
+
+bulkCreateNodes<-function(graph, labelName, indexName, indexValues){
+  query <- paste("CREATE (n:",labelName," {",indexName,":{indexValue}})", sep="");
+  t <- newTransaction(graph);
+  
+  for (i in 1:length(indexValues)){
+   value <- indexValues[i];
+   if (i==1) {print(query)}
+   appendCypher(t, query, indexValue = value);
+  }
+  
+  commit(t);
+  
+  addIndex(graph, labelName, indexName);
+}
+
+bulkUpdateNodeProperties<-function(graph, labelNames, indexNames, indexValues, 
+                                   propertyNames, propertyValues){
+  nodeInfo <- cbind(labelName=labelNames, indexName=indexNames, 
+                    indexValue=as.character(indexValues), 
+                    propertyName=propertyNames, 
+                    propertyValue=propertyValues);
+  t <- newTransaction(graph);
+  
+  for (i in 1:nrow(nodeInfo)){
+    query <- paste("MATCH (n:", nodeInfo[i,"labelName"]," {", nodeInfo[i,"indexName"], ":'", nodeInfo[i,"indexValue"], "'}) ",
+                   "SET n.",nodeInfo[i,"propertyName"],"='",nodeInfo[i,"propertyValue"], "'", sep="");
+    if (i==1) {print(query)}
+    appendCypher(t, query);
+  }
+
+  commit(t);
+}
+
+bulkCreateRelationships<-function(graph, 
+                                  startLabel, startIndexName, startIndexValue, 
+                                  endLabel, endIndexName, endIndexValue,
+                                  relationshipType){
+  relInfo <- cbind(startLabel, startIndexName, startIndexValue,
+                   endLabel, endIndexName, endIndexValue,
+                   relationshipType);
+  t <- newTransaction(graph);
+  for (i in 1:nrow(relInfo)){
+    query <- paste("MATCH (a:", relInfo[i,"startLabel"]," {", relInfo[i,"startIndexName"], ":'", relInfo[i,"startIndexValue"], "'}) ",
+                   "MATCH (b:", relInfo[i,"endLabel"]," {", relInfo[i,"endIndexName"], ":'", relInfo[i,"endIndexValue"], "'}) ",
+                   "CREATE (a)-[r:",relInfo[i,"relationshipType"],"]->(b)", sep="");
+    if (i==1) {print(query)}
+    appendCypher(t, query);
+  }
+  commit(t);
+}
---
+++ scrape_ckf.R
@@ -0,0 +1,74 @@
+#Extract and analyse menu items from the cheesecake factory menu
+getMenuItems <- function(url){
+  library(rvest);
+  library(stringr);
+  
+  #Save html from cheescake factory menu page
+  #"http://www.thecheesecakefactory.com/menu"
+  ckf_menu <- html(url);
+  
+  #Extract menu item titles based on css class
+  menu_titles <- ckf_menu %>%
+    html_nodes(".item-title") %>%
+    html_text();
+  
+  #Extract menu item descriptions based on css class
+  menu_descriptions <- ckf_menu %>%
+    html_nodes(".item-description") %>%
+    html_text();
+  
+  #Save titles and descriptions to data frame
+  ckf_items <- data.frame(name=menu_titles, description=menu_descriptions);
+  
+  #Remove apostrophes from title and descriptions (messes up indexing)
+  ckf_items$name<-str_replace_all(ckf_items$name,"'","");
+  ckf_items$description<-str_replace_all(ckf_items$description,"'","");
+  
+  #Remove whitespace from descriptions
+  ckf_items$description<-str_replace_all(ckf_items$description,"[\t\r\n]","");
+  
+  #Remove cutoff words at end of sentences
+  ckf_items$description<-str_replace_all(ckf_items$description,"\\s+\\w*\\W+$","")
+  
+  #Remove duplicate records
+  ckf_items<-unique(ckf_items);
+  ckf_items;
+}
+
+#Process menu title and descriptions into list of feautures
+getFeatures <- function(menu){
+  library(tm);
+  library(reshape);
+  
+  titles<-as.character(menu[,1]);
+  descriptions<-menu[,2];
+  
+  #stem descriptions
+  #descriptions<-stemDocument(descriptions);
+  
+  #convert to lower case
+  titles<-tolower(titles);
+  descriptions<-tolower(descriptions);
+  
+  #remove IP symbols from titles
+  titles<-str_replace_all(titles,"[™®]","");
+  
+  #append titles to front of descriptions to make key word list
+  features<-paste(titles,descriptions);
+  
+  #Remove punctuation
+  features<-removePunctuation(features);
+  
+  #Remove stop words
+  stopWords <- readLines(system.file("stopwords", "english.dat",package = "tm"));
+  features<-removeWords(features,stopWords);
+
+  #Stem and Tokenize
+  features<-lapply(features,scan_tokenizer);
+  features<-lapply(features,unique);
+  names(features)<-as.character(menu[,1]);
+  features<-melt.list(features);
+  names(features)<-c("feature", "item");
+  features$feature<-as.character(features$feature);
+  features;
+}
