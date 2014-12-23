29011634236473b44922ab1a8b936b1f89e48412
Brian Loyal <bloyal@gmail.com>
2014-12-22 21:06:40 -0600

    Get features for menu item

--- queryCheesecakeFactoryGraph.R
+++ queryCheesecakeFactoryGraph.R
@@ -7,7 +7,18 @@ beerNode <- getLabeledNodes(graph, "MenuItem", name = "Beer");
 
 beerRels <- sapply(beerNode, outgoingRels);
 
+wineFeatures <- getFeaturesForMenuItem(graph, "Wine");
+
 wineConnections <- getRelatedMenuItems(graph, "Wine");
+
+getFeaturesForMenuItem <- function(graph, itemName){
+  query <- "MATCH (a:MenuItem {name:{name}})-[r:HAS_FEATURE]->(b:Feature)
+            RETURN DISTINCT b.name
+            ORDER BY b.name";
+  results<-cypher(graph, query, name=itemName);  
+  names(results)<-c("features");
+  results;
+}
 
 getRelatedMenuItems <- function(graph, itemName){
   query <- "MATCH (a:MenuItem {name:{name}})-->(b:Feature)<--(c:MenuItem)
