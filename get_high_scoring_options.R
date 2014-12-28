4e4eca6d61199093818150f9e7551cdb0d3ca24b
Brian Loyal <bloyal@gmail.com>
2014-12-28 17:46:48 -0600

    almost there with score function

--- get_high_scoring_options.R
+++ get_high_scoring_options.R
@@ -1,7 +1,21 @@
 #get high scoring options related to a node
 
 getHighScoringOptions <- function(graph, session, itemName, maxItems = 2){
-  print(maxItems)
+#   query <- paste("MATCH (a:MenuItem {name:{name}})-->(b:Feature)<--(c:MenuItem)
+#             WITH DISTINCT c 
+#             WITH c, rand() as r 
+#             RETURN c.name 
+#             ORDER BY r 
+#             LIMIT ", maxItems, sep="")
+  query <- paste("MATCH (a:MenuItem {name:{name}})-->(f:Feature)<--(c:MenuItem),
+            (s:Session {id:12})-[r:HAS_AFFINITY_FOR]->(f)
+            RETURN a.name as choice_name, c.name as related_item_name, sum(r.score) as affinity_score
+            ORDER BY a.name, affinity_score desc
+            LIMIT ", maxItems, sep="");
+  print(query)
+  results<-cypher(graph, query, name=itemName);  
+  names(results)<-c("name");
+  results;
 }
 
 #get affinity scores
@@ -14,3 +28,9 @@ getHighScoringOptions <- function(graph, session, itemName, maxItems = 2){
 # MATCH (a:MenuItem {name:"Lemon Drop"})-->(b:Feature)<--(c:MenuItem)
 # RETURN DISTINCT a.name, b.name, c.name
 # ORDER BY c.name
+
+#Get related menu items, ordered by sum of affinity scores
+# MATCH (a:MenuItem {name:"Lemon Drop"})-->(f:Feature)<--(c:MenuItem),
+# (s:Session {id:12})-[r:HAS_AFFINITY_FOR]->(f)
+# RETURN a.name as choice_name, c.name as related_item_name, sum(r.score) as affinity_score
+# ORDER BY a.name, affinity_score desc
